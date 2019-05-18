cdeef03ac0efef8a4a8a4723259ca31915334e09
Cristian <cristian.labadie@gmail.com>
2019-05-18 18:57:59 -0400

    Update API Models
    Adding models:
    
    Album, Song

---
+++ models/song.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var SongSchema = Schema({
+  number: String,
+  name: String,
+  file: String,
+  duration: String,
+  image: String,
+  album: { type: Schema.ObjectId, ref: 'Album'}
+});
+
+
+module.exports = mongoose.model('Song', SongSchema);
