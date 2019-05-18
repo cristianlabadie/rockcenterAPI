1b9b617d63477cba131b7814fe87ace62f3306f4
Cristian <cristian.labadie@gmail.com>
2019-05-18 18:48:48 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3977;
+
+mongoose.connect('mongodb://localhost:27017/rockcenter', (err, res) =>{
+  if(err){
+    throw err;
+  }else{
+    console.log("La conexion a la base de datos esta correcta...");
+
+    app.listen(port, function(){
+      console.log("Servidor api rest escuchando en http://localhost:"+port);
+    })
+  }
+});
